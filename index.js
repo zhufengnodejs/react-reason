b79f7213b931f0cecde31a86af9eb28ea8bf1bcc
zhufengnodejs <zhufengnodejs@126.com>
2017-05-08 21:37:20 +0800

    3.生成DOM元素并添加事件

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
+const createDOMFromString = function(domString){
+  const div = document.createElement('div');
+  div.innerHTML = domString;
+  return div.children[0];
+}
+
 class LikeButton{
+    constructor(){
+        this.state = {isLiked:false};
+    }
+    handleClick(){
+        let btnTxt = document.querySelector('.like-text');
+        this.state.isLiked = !this.state.isLiked;
+        if(this.state.isLiked)
+            btnTxt.innerHTML = '取消';
+        else
+            btnTxt.innerHTML = '点赞';
+    }
     render(){
-        return (
-            `
+        this.ele = createDOMFromString(`
              <button class="like-btn">
                 <span class="like-text">点赞</span>
              </button>
-            `
-        )
+            `);
+        this.ele.addEventListener('click',this.handleClick.bind(this));
+        return this.ele;
     }
 }
