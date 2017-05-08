b000101f3637633949f7203cc4d4038bd24f999b
zhufengnodejs <zhufengnodejs@126.com>
2017-05-08 22:07:25 +0800

    5. 抽象出Component类并支持属性

--- index.js
+++ index.js
@@ -1,32 +1,46 @@
-const createDOMFromString = function(domString){
-  const div = document.createElement('div');
-  div.innerHTML = domString;
-  return div.children[0];
-}
-
-class LikeButton{
-    constructor(){
-        this.state = {isLiked:false};
+class Component{
+    constructor(props){
+        this.props = props;
+    }
+    createDOMFromString(domString){
+        const div = document.createElement('div');
+        div.innerHTML = domString;
+        return div.children[0];
     }
     setState(state){
         this.state = state;
         let oldEle = this.ele;
         let parent = oldEle.parentElement;
-        this.render();
+        this.getElement();
         parent.replaceChild(this.ele,oldEle);
     }
+    getElement(){
+        this.ele = this.createDOMFromString(this.render());
+        this.ele.addEventListener('click',this.handleClick.bind(this));
+    }
+    $mount(container){
+        this.getElement();
+        container.appendChild(this.ele);
+    }
+}
+class LikeButton extends Component{
+    constructor(props){
+        super(props);
+        this.state = {isLiked:false};
+    }
+
     handleClick(){
         this.setState({
             isLiked:!this.state.isLiked
         })
     }
     render(){
-        this.ele = createDOMFromString(`
+        return (
+            `
              <button class="like-btn">
-                <span class="like-text">${this.state.isLiked?'点赞':'取消'}</span>
+                <span class="like-text">为${this.props.name} ${this.state.isLiked?'取消':'点赞'}</span>
              </button>
-            `);
-        this.ele.addEventListener('click',this.handleClick.bind(this));
-        return this.ele;
+            `
+        )
     }
 }
